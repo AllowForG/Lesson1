1648502690a6ad0b506b164c711535e1c9d70fda
AllowForG <81081748+AllowForG@users.noreply.github.com>
2021-04-15 07:25:50 -0400

    Update Lesson1-Task1.rb

--- Lesson1-Task1.rb
+++ Lesson1-Task1.rb
@@ -1,13 +1,12 @@
-puts "What is your name ? "
-name = gets.chomp
-
-puts "What is your height ?"
-height = gets.chomp.to_i
-x = 1.15
-ideal_weight = (height -110)*x
-if ideal_weight < 0
-    puts "#{name} your weight is optimal"
-else
-    puts "#{name}, your ideal weight is #{ideal_weight.to_i} KG"
+puts "What is your name ?"
+  name = gets.chomp
+    puts "What is your height ?"
+     height = gets.chomp.to_i
+      x = 1.15
+       ideal_weight = (height - 110) * x
+      if ideal_weight < 0
+     puts "#{name} your weight is optimal"
+   else
+  puts "#{name}, your ideal weight is #{ideal_weight.to_i} KG"
 end
     
