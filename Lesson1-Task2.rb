85e6859ba9fa3de86f2b981ab688eddc50236830
AllowForG <81081748+AllowForG@users.noreply.github.com>
2021-04-15 07:40:04 -0400

    Update Lesson1-Task2.rb

--- Lesson1-Task2.rb
+++ Lesson1-Task2.rb
@@ -3,8 +3,13 @@ puts "Set initial parametrs of the triangle"
     a = gets.to_f
      puts "Height"
        h = gets.to_f
+
        if a < 0 && h < 0
+         
       puts "BAD WORK"
+         
     elsif a > 0 && h > 0
+         
   puts "The area of the triangle is #{1 / 2.0 * a * h}"
- end
+         
+end
