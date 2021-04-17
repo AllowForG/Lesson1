354f37494ee40bed5da2394f7c51f10bba04c7e7
AllowForG <81081748+AllowForG@users.noreply.github.com>
2021-04-17 03:07:56 -0400

    Update Lesson1-Task4.rb

--- Lesson1-Task4.rb
+++ Lesson1-Task4.rb
@@ -7,14 +7,14 @@ puts "c = ?"
   c = gets.to_f
 
 D = b**2 - 4 * a * c
-  if D > 0
-puts "x1 = #{(-b + Math.sqrt(D)) / 2 * a}"
+if D > 0
+  puts "x1 = #{(-b + Math.sqrt(D)) / 2 * a}"
     
-puts "x2 = #{(-b - Math.sqrt(D)) / 2 * a}"
-  elsif D == 0
-puts "x = #{-b /2 * a}"
-  else D < 0
-puts "No roots"
+  puts "x2 = #{(-b - Math.sqrt(D)) / 2 * a}"
+elsif D == 0
+  puts "x = #{-b /2 * a}"
+else D < 0
+  puts "No roots"
     
 end
    
