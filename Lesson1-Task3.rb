232cf7ba49d5fe3a5d9927cfd80b5c95cd17bcdc
AllowForG <81081748+AllowForG@users.noreply.github.com>
2021-04-17 03:07:10 -0400

    Update Lesson1-Task3.rb

--- Lesson1-Task3.rb
+++ Lesson1-Task3.rb
@@ -1,26 +1,18 @@
 puts "Set initial parametrs of the triangle"
-  puts "a = ?"
-    a = gets.to_f
-      puts "b = ?"
-        b = gets.to_f
-          puts "c = ?"
-            c = gets.to_f
+puts "a = ?"
+  a = gets.to_f
+puts "b = ?"
+  b = gets.to_f
+puts "c = ?"
+  c = gets.to_f
 
-if c**2 == a**2 + b**2
-                 
-  puts "Rectangular triangle"
-                 
-    elsif c != a && b == a
-                 
-      puts "Isosceles triangle"
-                 
-        elsif b == c && c == a
-                 
-          puts "Equilateral triangle"
-                 
-            else
-                 
-              puts "Chto ti nadelal ?"
-                 
-                end
+if c**2 == a**2 + b**2              
+  puts "Rectangular triangle"             
+elsif c != a && b == a
+  puts "Isosceles triangle"
+elsif b == c && c == a               
+  puts "Equilateral triangle"              
+else
+  puts "Chto ti nadelal ?"
+end
     
