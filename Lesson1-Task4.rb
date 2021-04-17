b853aac576349c4d6714b440697314af8f41c42a
AllowForG <81081748+AllowForG@users.noreply.github.com>
2021-04-17 03:05:17 -0400

    Update Lesson1-Task4.rb

--- Lesson1-Task4.rb
+++ Lesson1-Task4.rb
@@ -1,26 +1,21 @@
 puts "Solving quadratic equations"
-  puts "a = ?"
-    a = gets.to_f
-      puts "b = ?"
-        b = gets.to_f
-          puts "c = ?"
-            c = gets.to_f
+puts "a = ?"
+  a = gets.to_f
+puts "b = ?"
+  b = gets.to_f
+puts "c = ?"
+  c = gets.to_f
 
 D = b**2 - 4 * a * c
   if D > 0
-     
-    puts "x1 = #{(-b + Math.sqrt(D)) / 2 * a}"
-     
-      puts "x2 = #{(-b - Math.sqrt(D)) / 2 * a}"
-     
-        elsif D == 0
-     
-          puts "x = #{-b /2 * a}"
-     
-           else D < 0
-     
-             puts "No roots"
-     
-               end
+puts "x1 = #{(-b + Math.sqrt(D)) / 2 * a}"
+    
+puts "x2 = #{(-b - Math.sqrt(D)) / 2 * a}"
+  elsif D == 0
+puts "x = #{-b /2 * a}"
+  else D < 0
+puts "No roots"
+    
+end
    
    
